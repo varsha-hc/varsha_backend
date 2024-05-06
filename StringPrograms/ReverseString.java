c808015b457055bdccf71700f118fbe566cef557
Varsha H C. <varsha.hc@excelindia.com>
2024-05-06 18:39:07 +0530

    String Programs

---
+++ StringPrograms/ReverseString.java
@@ -0,0 +1,9 @@
+public class ReverseString {
+    public static void main(String[] args) {
+        String str = "Hello World";
+        String result = "";
+        for(int i= str.length()-1; i>=0; i--)
+        result+=str.charAt(i);
+        System.out.println(result);
+    }
+}
